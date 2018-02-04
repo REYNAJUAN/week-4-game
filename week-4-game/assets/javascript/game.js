67b3e49304206295d681aa2806af557b59205e23
REYNAJUAN <31169278+REYNAJUAN@users.noreply.github.com>
2018-02-03 23:06:28 -0800

    Manual upload
    Used flex box but confused if should use regular row with images. 
    will kep working on functionally (keep testing onclick/keyup/parseint)

---
+++ week-4-game/assets/javascript/game.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+var numberToMatch=getNumberToMatch();
+var yourNumber="";
+var guesses=['5,50'];
+
+
+$("#.buttons" ).click(function() {
+    $( this ).slideUp();
+    function getNumberToMatch(){
+        var guesses="numbers";
+        return (value);
+console.log(numberToMatch);
+console.log(yourNumber);
+
+};
+
+
+
+
+
+/*var yourNumber= document.getElementById('yourNumber');
+yourNumber.innerHTML=0;
+
+var numberToMatch= document.getElementById('numberToMatch');
+numberToMatch.innerHTML= getRandomValue(0,25)
+
+var value=[5,10,15];
+
+function addToScore(value){
+    var numberToAdd=parseInt(value);
+    var currentScore=parseInt(yourNumber.innerHTML);
+    wins.innerHTML=numberToAdd+currentScore;
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
